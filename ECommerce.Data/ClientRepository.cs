5e37cfa3d4e8f497102b1a505d4acc04648c78ae
fernando s pinto <pintofs1@hotmail.com>
2019-07-27 09:32:21 -0300

    client repository

---
+++ ECommerce.Data/ClientRepository.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using ECommerce.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECommerce.Data
+{
+    public class ClientRepository : Repository<Client>, IClientRepository
+    {
+        public List<ClientRepository> GetByCPF(string cpf)
+        {
+            return _connection.Query<ClientRepository>($@"Select * from Client where brand like '%{cpf}%' ").ToList();
+        }
+
+        public void Add(ClientRepository obj)
+        {
+            _connection.Execute("Insert Into Client (Name, Email, CPF) Values(@name, @email, @cpf)", obj);
+        }
+    }
+}
