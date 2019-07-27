3d73c51f3fa42ffb69f377e0dcabfb128d7a363e
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-27 16:00:49 -0300

    Create Product business

---
+++ ECommerce.Business/IProductBusiness.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Business
+{
+    public interface IProductBusiness
+    {
+        void Add(Product product);
+        Product GetProductById(int id);
+        List<Product> GetAll();
+    }
+}
---
+++ ECommerce.Business/ProductBusiness.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECommerce.Data;
+using ECommerce.Domain;
+
+namespace ECommerce.Business
+{
+    public class ProductBusiness : IProductBusiness
+    {
+        private readonly IProductRepository _ProductRepository;
+
+        public ProductBusiness(IProductRepository iProductRepository)
+        {
+            _ProductRepository = iProductRepository;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _ProductRepository.GetAll();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _ProductRepository.GetById(id);
+        }
+
+        public void Add(Product product)
+        {
+            _ProductRepository.Add(product);
+        }
+    }
+}
