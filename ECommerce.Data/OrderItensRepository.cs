6f6029dae495a968bfae0a953a35985c955aa231
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-27 11:10:59 -0300

    Add OrderItensRepository

---
+++ ECommerce.Data/OrderItensRepository.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using ECommerce.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Data
+{
+    public class OrderItensRepository : Repository<OrderItens>, IOrderItensRepository
+    {
+        public void Add(OrderItensRepository obj)
+        {
+            _connection.Execute("Insert Into OrderItens (product, quantity) Values(@product, @quantity)", obj);
+        }
+        public List<OrderItensRepository> GetAll()
+        {
+            return _connection.Query<OrderItensRepository>($@"Select * from Order").ToList();
+        }
+    }
+}
