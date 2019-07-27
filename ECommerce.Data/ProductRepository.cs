4723d4e0b47450974f176d64a05ad35ca1f3abbc
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-27 10:28:30 -0300

    Product repository add

---
+++ ECommerce.Data/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using ECommerce.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Data
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public void Add(ProductRepository obj)
+        {
+            _connection.Execute("Insert Into Product (Name, Value, Quantity, ProductType) Values(@name, @value, @quantity, @productType)", obj);
+        }
+        public List<ProductRepository> GetAll()
+        {
+            return _connection.Query<ProductRepository>($@"Select * from Product").ToList();
+        }
+
+    }
+}
