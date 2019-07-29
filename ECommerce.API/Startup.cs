d53ca8cb77a899221de970f15d2330efcc4f28dc
fernando s pinto <pintofs1@hotmail.com>
2019-07-29 20:45:20 -0300

    configuration injeção de dependencia

--- ECommerce.API/Startup.cs
+++ ECommerce.API/Startup.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Business;
 using ECommerce.Data;
+using ECommerce.Service;
+using ECommerce.Service.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -35,10 +38,37 @@ namespace ECommerce.API
 
         public void DependencyInjection(IServiceCollection services)
         {
-            var conn = this.Configuration.GetConnectionString("DefaultConnection");
+            //var conn = this.Configuration.GetConnectionString("DefaultConnection");
 
-            var clientRepository = new ClientRepository(Configuration);
-            services.AddSingleton<IClientRepository, ClientRepository>();   
+            //var clientRepository = new ClientRepository(Configuration);
+
+            #region repository
+            services.AddSingleton<IClientRepository, ClientRepository>();
+            services.AddSingleton<IOrderItensRepository, OrderItensRepository>();
+            services.AddSingleton<IOrderRepository, OrderRepository>();
+            services.AddSingleton<IProductRepository, ProductRepository>();
+            services.AddSingleton<IProductTypeRepository, ProductTypeRepository>();
+            #endregion repository
+
+            #region business
+            services.AddSingleton<IClientBusiness, ClientBusiness>();
+            services.AddSingleton<IOrderItensBusiness, OrderItensBusiness>();
+            services.AddSingleton<IOrderBusiness, OrderBusiness>();
+            services.AddSingleton<IProductBusiness, ProductBusiness>();
+            services.AddSingleton<IProductTypeBusiness, ProductTypeBusiness>();
+            #endregion business
+
+            #region service
+            services.AddSingleton<IClientService, ClientService>();
+            services.AddSingleton<IOrderItensService, OrderItensService>();
+            services.AddSingleton<IOrderService, OrderService>();
+            services.AddSingleton<IProductService, ProductService>();
+            services.AddSingleton<IProductTypeService, ProductTypeService>();
+            #endregion business
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
