f6d35ccce6c50edd9046ed4c1e84e869bc3b76e2
Alexandre Canaã <acanaaol@gmail.com>
2019-07-29 22:40:35 -0300

    Add Swagger

--- ECommerce.API/Controllers/ProductController.cs
+++ ECommerce.API/Controllers/ProductController.cs
@@ -12,43 +12,34 @@ namespace ECommerce.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
-    {
+    public class ProductController : ControllerBase {
         private readonly IProductService _productService;
-        public ProductController(IProductService productService)
-        {
+        public ProductController(IProductService productService) {
             _productService = productService;
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Product>> Get()
-        {
+        [HttpGet("product/{all}")]
+        public ActionResult<IEnumerable<Product>> Get() {
             return _productService.GetAll();
         }
 
-        [HttpGet]
-        public ActionResult<Product> Get(string name)
-        {
+        [HttpGet("product/{name}")]
+        public ActionResult<Product> Get(string name) {
             return _productService.GetByName(name);
         }
 
-        [HttpPost]
-        public ActionResult Post([FromBody]Product product)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
+        [HttpPost("product/{post}")]
+        public ActionResult Post([FromBody]Product product) {
+            try {
+                if (ModelState.IsValid) {
                     _productService.Add(product);
                     return Ok("success");
                 }
-                else
-                {
+                else {
                     return BadRequest(ModelState);
                 }
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return new StatusCodeResult(500);
             }
         }
