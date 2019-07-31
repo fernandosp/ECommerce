0006f081bd378fd7bd0c952f1163729614d6aa14
Alexandre Canaã <acanaaol@gmail.com>
2019-07-30 21:53:48 -0300

    OrderItems Business Implementation

--- ECommerce.Business/OrderItensBusiness.cs
+++ ECommerce.Business/OrderItensBusiness.cs
@@ -3,24 +3,68 @@ using System.Collections.Generic;
 using System.Text;
 using ECommerce.Data;
 using ECommerce.Domain;
+using System.Linq;
 
 namespace ECommerce.Business
 {
     public class OrderItensBusiness : IOrderItensBusiness
     {
         private readonly IOrderItensRepository _IOrderItensRepository;
-        public OrderItensBusiness(IOrderItensRepository iOrderItensRepository)
+        private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
+
+
+        public OrderItensBusiness(IOrderItensRepository iOrderItensRepository, 
+            IOrderRepository orderRepository, 
+            IProductRepository productRepository)
         {
             _IOrderItensRepository = iOrderItensRepository;
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
         }
-        public OrderItens Add(OrderItens orderItens)
+        public OrderItens Add(OrderItens orderItens, int id_order)
         {
-            return _IOrderItensRepository.Add(orderItens);
+            Order order = null;
+            Product product = null;
+
+            OrderItens orderItensDB;
+
+            order = _orderRepository.GetById(id_order);
+
+            if(order == null) {
+                throw new Exception("Pedido Inexistente");
+            }
+
+            if (order.OrderStatus != OrderStatus.ABERTA) {
+                throw new Exception("Pedido Deve Estar Aberto");
+            }
+
+            product = _productRepository.GetById(orderItens.Products.Id);
+
+            if(product == null) {
+                throw new Exception("Produto Não Existe");
+            }
+
+            if(product.Quantity >= orderItens.Quantity) {
+                throw new Exception("Quantidade do Produto Indisponível");
+            }
+
+            //orderItensDB = _IOrderItensRepository.
+
+            ////if (orderItens.Quantity < product.Quantity) {
+            ////    throw new Exception("Estoque Insuficiente do Produto");
+            ////}
+
+            //if (order.OrderItens.Where(x => x.Products == product))
+
+            return _iorderitensrepository.add(orderitens);
         }
 
         public List<OrderItens> GetAll()
         {
             return _IOrderItensRepository.GetAll();
         }
+
+        
     }
 }
